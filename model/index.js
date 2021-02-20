4c7228899e7c8e03f5809b08a1f270beaf5e6855
Warness <65484013+Warness@users.noreply.github.com>
2021-02-20 18:03:08 +0200

    1

--- model/index.js
+++ model/index.js
@@ -1,20 +1,97 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const path = require('path')
 
-const listContacts = async () => {}
+const contactsPathName = path.resolve(__dirname, '/.contacts.json')
+console.log()
+const readFile = async pathToFile => {
+  try {
+    const file = await fs.readFile(pathToFile, 'utf-8')
+    return JSON.parse(file)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-const getContactById = async (contactId) => {}
+// const writeFile = async data => {
+//   try {
+//     fs.writeFile(contactsPathName, data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
-const removeContact = async (contactId) => {}
+// const getContacts = async () => {};
 
-const addContact = async (body) => {}
+async function getContactsList() {
+  const contactsObj = await readFile(contactsPathName)
+  console.log(contactsObj)
+  return contactsObj
+}
+
+const getContactById = async contactId => {}
+
+const removeContact = async contactId => {}
+
+const addContact = async body => {}
 
 const updateContact = async (contactId, body) => {}
 
 module.exports = {
-  listContacts,
+  getContactsList,
   getContactById,
   removeContact,
   addContact,
   updateContact,
 }
+
+// async function getContactById(contactId) {
+//   try {
+//     const contactsList = await readFile(contactsPathName);
+//     const contactItem = contactsList.filter((e) => e.id === Number(contactId));
+//     console.log(contactItem[0]);
+//     return contactItem[0];
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// async function removeContact(contactId) {
+//   try {
+//     const contactsList = await readFile(contactsPathName);
+//     const newContactsList = contactsList.filter(
+//       (e) => e.id !== Number(contactId)
+//     );
+//     const data = JSON.stringify(newContactsList);
+
+//     await writeFile(data);
+//     console.log(`Contact removed`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// async function addContact(name, email, phone) {
+//   try {
+//     const contactsList = await readFile(contactsPathName);
+//     const newContactObj = {
+//       name,
+//       email,
+//       phone,
+//       id: Math.floor(Math.random() * Math.floor(1000000000)),
+//     };
+//     contactsList.push(newContactObj);
+//     const data = JSON.stringify(contactsList);
+//     await fs.writeFile(contactsPathName, data);
+//     console.log("Contact added");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+// };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,23 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: "method get '/'" })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'method get"/:contactId"' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'method post' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'method delete' })
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'method patch' })
 })
 
 module.exports = router
