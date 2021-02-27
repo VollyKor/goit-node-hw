97f4d699d744c885e87ab47621964e1aebfc03d4
Warness <65484013+Warness@users.noreply.github.com>
2021-02-27 18:34:04 +0200

    homework №3 complete
    connect mongoDB with mongoose to project

---
+++ model/schemas/contact.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const SUBSCRIPTION = {
+    FREE: 'free',
+    PRO: 'pro',
+    PREMIUM: 'premium',
+}
+
+const contactSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Set name for contact'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Set email for contact'],
+        },
+        phone: {
+            type: String,
+            required: [true, 'Set phone for contact'],
+            unique: true,
+        },
+
+        date: { type: Date, default: () => Date.now() },
+
+        subscription: {
+            type: String,
+            enum: [SUBSCRIPTION.FREE, SUBSCRIPTION.PRO, SUBSCRIPTION.PREMIUM],
+            default: SUBSCRIPTION.FREE,
+        },
+        password: {
+            type: String,
+            default: 'password',
+        },
+        token: {
+            type: String,
+            default: '',
+        },
+    },
+    { versionKey: false, timestamps: true },
+)
+
+const Contact = model('contact', contactSchema)
+module.exports = Contact
